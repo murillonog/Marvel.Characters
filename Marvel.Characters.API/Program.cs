92b7c357a6aaabcb81787bed129aedf4b02d8062
Murillo Nogueira <murillo.nogueira01@outlook.com>
2022-11-11 14:55:36 -0300

    Created structure to swagger

--- Marvel.Characters.API/Program.cs
+++ Marvel.Characters.API/Program.cs
@@ -4,6 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
 
 builder.Services.AddInfrastructureAPI(configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddInfrastructureSwagger();
 
 var app = builder.Build();
+
+app.MapGet("/", () => "Olá Mundo!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.Run();
---
+++ Marvel.Characters.Infra.IoC/DependencyInjectionSwagger.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Marvel.Characters.Infra.IoC
+{
+    public static class DependencyInjectionSwagger
+    {
+        public static IServiceCollection AddInfrastructureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Marvel Characters API", Version = "v1" });
+            });
+
+            return services;
+        }
+    }
+}
