4ebfe22d29b5b7f2505d0b88348055fc2b98b29b
Murillo Nogueira <murillo.nogueira01@outlook.com>
2022-11-11 09:13:43 -0300

    Created context class ApplicationDbContext, using EntityFrameWorkCore

---
+++ Marvel.Characters.Infra.IoC/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Marvel.Characters.Infra.IoC.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
