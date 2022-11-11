c1e8623cb448afb0c5a5ee4756ff434093f74b2d
Murillo Nogueira <murillo.nogueira01@outlook.com>
2022-11-11 10:31:41 -0300

    Chamando a classe DependencyInjection em program.cs and created the connection string

--- Marvel.Characters.API/Program.cs
+++ Marvel.Characters.API/Program.cs
@@ -1,6 +1,9 @@
+using Marvel.Characters.Infra.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddInfrastructureAPI(configuration);
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
 app.Run();
