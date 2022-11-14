26599b783d5693dfcdca9c4720542b8ad5dfd7be
Murillo Nogueira <murillo.nogueira01@outlook.com>
2022-11-14 17:36:27 -0300

    Create sync route

---
+++ Marvel.Characters.API/Controllers/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marvel.Characters.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        [HttpPut("sync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(List<string>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(List<string>))]   
+        public async Task<IActionResult> Sync()
+        {
+            return null;
+        }
+    }
+}
--- Marvel.Characters.API/Program.cs
+++ Marvel.Characters.API/Program.cs
@@ -3,6 +3,7 @@ using Marvel.Characters.Infra.IoC;
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
 
+builder.Services.AddControllers();
 builder.Services.AddInfrastructureAPI(configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddInfrastructureSwagger();
@@ -15,6 +16,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 
 app.Run();
